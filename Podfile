a2593d9b5f5237a94193a878f2cbe5bdba8e4089
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2020-10-13 18:32:15 +0700

    Added Podfile (Linear Progress) Added Progress View in Main Screen Added Navigation Bar (Large Titles)

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iwaste-ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'LinearProgressView', '~> 2.0'
+
+  # Pods for iwaste-ios
+
+end
--- iwaste-ios/ViewController.swift
+++ iwaste-ios/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import LinearProgressView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var linearProgressPlastics: LinearProgressView!
+    @IBOutlet weak var linearProgressMetals: LinearProgressView!
+    @IBOutlet weak var linearProgressGlass: LinearProgressView!
+    @IBOutlet weak var linearProgressPapers: LinearProgressView!
+    @IBOutlet weak var linearProgressOrganic: LinearProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        linearProgressPlastics.animationDuration = 0.5
+        linearProgressPlastics.barColor = #colorLiteral(red: 0.4632664323, green: 0.7548244596, blue: 0.8504111171, alpha: 1)
+        linearProgressPlastics.trackColor = #colorLiteral(red: 0, green: 0.5076726079, blue: 0.6999972463, alpha: 1)
+        
+        linearProgressMetals.animationDuration = 0.5
+        linearProgressMetals.barColor = #colorLiteral(red: 0.9633008838, green: 0.4755309224, blue: 0.4895489812, alpha: 1)
+        linearProgressMetals.trackColor = #colorLiteral(red: 0.8784641623, green: 0, blue: 0, alpha: 1)
+        
+        linearProgressGlass.animationDuration = 0.5
+        linearProgressGlass.barColor = #colorLiteral(red: 0.7479692101, green: 0.5221053958, blue: 0.9678519368, alpha: 1)
+        linearProgressGlass.trackColor = #colorLiteral(red: 0.470867455, green: 0, blue: 0.9179152846, alpha: 1)
+        
+        linearProgressPapers.animationDuration = 0.5
+        linearProgressPapers.barColor = #colorLiteral(red: 1, green: 0.840634346, blue: 0.4565974474, alpha: 1)
+        linearProgressPapers.trackColor = #colorLiteral(red: 1, green: 0.6840727925, blue: 0, alpha: 1)
+        
+        linearProgressOrganic.animationDuration = 0.5
+        linearProgressOrganic.barColor = #colorLiteral(red: 0.4335449338, green: 0.7922176719, blue: 0.4929479361, alpha: 1)
+        linearProgressOrganic.trackColor = #colorLiteral(red: 0, green: 0.5792904496, blue: 0, alpha: 1)
+        
+        updateLinearProgress()
     }
-
-
+    
+    @objc
+    func updateLinearProgress() {
+        linearProgressPlastics.setProgress(20, animated: true)
+        linearProgressPlastics.barInset = CGFloat(4)
+        linearProgressPlastics.isCornersRounded = true
+        
+        linearProgressMetals.setProgress(40, animated: true)
+        linearProgressMetals.barInset = CGFloat(4)
+        linearProgressMetals.isCornersRounded = true
+        
+        linearProgressGlass.setProgress(60, animated: true)
+        linearProgressGlass.barInset = CGFloat(4)
+        linearProgressGlass.isCornersRounded = true
+        
+        linearProgressPapers.setProgress(35, animated: true)
+        linearProgressPapers.barInset = CGFloat(4)
+        linearProgressPapers.isCornersRounded = true
+        
+        linearProgressOrganic.setProgress(85, animated: true)
+        linearProgressOrganic.barInset = CGFloat(4)
+        linearProgressOrganic.isCornersRounded = true
+    }
 }
 
