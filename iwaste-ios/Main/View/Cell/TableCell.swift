2c5b389282227152506d28dbe75b36c2fea1c914
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2020-10-20 13:21:04 +0700

    Added Button Check in Achievement Screen

--- iwaste-ios/Main/View/Cell/TableCell.swift
+++ iwaste-ios/Main/View/Cell/TableCell.swift
@@ -13,13 +13,26 @@ class TableCell: UITableViewCell {
     @IBOutlet weak var coinLabel: UILabel!
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var btnCheckBox: UIButton!
     
+    @IBAction func btnCheckBoxTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            
+        }) { (success) in
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.isSelected = !sender.isSelected
+                sender.transform = .identity
+            }, completion: nil)
+        }
+    }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         titleLabel.text = nil
         coinLabel.text = nil
         iconImage.image = nil
+
     }
     
     func configurateTheCell(_ challenge: Challenge) {
@@ -27,6 +40,8 @@ class TableCell: UITableViewCell {
         coinLabel.text = challenge.coin
         iconImage.image = UIImage(named: challenge.image)
         subView.backgroundColor =  #colorLiteral(red: 1, green: 0.9176470588, blue: 0.9098039216, alpha: 1)
+        btnCheckBox.setImage(UIImage(named:"Checkmarkempty"), for: .normal)
+        btnCheckBox.setImage(UIImage(named:"Checkmark"), for: .selected)
         
         subView.layer.cornerRadius = 20
         subView.layer.borderWidth = 0.1
