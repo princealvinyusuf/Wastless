0e186e410a185096c117479bf8a37875a8f7d37e
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2020-10-20 10:46:17 +0700

    Added Square Icon Build Achievement Layout Import Achievement Asset

---
+++ iwaste-ios/Main/Model/Challenge.swift
@@ -0,0 +1,8 @@
+//
+//  Challenge.swift
+//  iwaste-ios
+//
+//  Created by Prince Alvin Yusuf on 20/10/20.
+//
+
+import Foundation
---
+++ iwaste-ios/Main/View/Cell/TableCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  iwaste-ios
+//
+//  Created by Prince Alvin Yusuf on 20/10/20.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
