c51b42c6d57cc824f6aae4eb48692af68c410cd4
Fauzi <hilmy2602@gmail.com>
2020-10-27 11:51:28 +0700

    MVP refactor for set target screen

--- iwaste-ios/Main/Presenter/SetTargetPresenter.swift
+++ iwaste-ios/Main/Presenter/SetTargetPresenter.swift
@@ -5,6 +5,55 @@
 //  Created by Muhammad Hilmy Fauzi on 27/10/20.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
-
+class SetTargetPresenter {
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    func addCategoryTarget(name: String, target: Int) {
+        if let appDelegate = appDelegate {
+            let managedContext = appDelegate.persistentContainer.viewContext
+            
+            let catEntity = NSEntityDescription.entity(forEntityName: "CategoryCD", in: managedContext)
+            
+            let trashCategory = NSManagedObject(entity: catEntity!, insertInto: managedContext)
+            
+            trashCategory.setValue(Date(), forKey: "date")
+            trashCategory.setValue(target, forKey: "target")
+            trashCategory.setValue(name, forKey: "name")
+            
+            do {
+                try managedContext.save()
+            } catch let err {
+                print("error save category data: ", err)
+            }
+        }
+    }
+    
+    func addDataTrash() {
+        if let appDelegate = appDelegate {
+            let managedContext = appDelegate.persistentContainer.viewContext
+            
+            let trashEntity = NSEntityDescription.entity(forEntityName: "TrashCD", in: managedContext)
+            
+            for singleWaste in dataWaste {
+                let waste = singleWaste.waste
+                for n in 0...waste.count-1 {
+                    let newTrash = NSManagedObject(entity: trashEntity!, insertInto: managedContext)
+                    
+                    newTrash.setValue(Date(), forKey: "date")
+                    newTrash.setValue(singleWaste.categoryName, forKey: "type")
+                    newTrash.setValue(waste[n].wasteName, forKey: "name")
+                    
+                    do {
+                        try managedContext.save()
+                    } catch let err {
+                        print("error save trash data: ", err)
+                    }
+                }
+            }
+            
+        }
+    }
+}
--- iwaste-ios/Main/View/SetTargetVC.swift
+++ iwaste-ios/Main/View/SetTargetVC.swift
@@ -9,37 +9,29 @@ import UIKit
 import CoreData
 
 class SetTargetVC: UIViewController {
-
+    
     //UI
     @IBOutlet weak var plasticTF: UITextField!
     @IBOutlet weak var glassTF: UITextField!
     @IBOutlet weak var paperTF: UITextField!
     @IBOutlet weak var metalTF: UITextField!
     @IBOutlet weak var organicTF: UITextField!
-    @IBAction func btnDone(_ sender: UIBarButtonItem) {
-        // set to true so target screen wont show up after target is set
-        UserDefaultService.instance.isTargetSet = true
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    //CoreData
+    
+    var presenter: SetTargetPresenter?
     var delegate: WasteTargetDelegate?
     
-    var categoy = [CategoryCD]()
-    var managedObjectContext: NSManagedObjectContext?
-    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    var category = [CategoryCD]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        managedObjectContext = appDelegate?.persistentContainer.viewContext
-
+        presenter = SetTargetPresenter()
     }
+    
     @IBAction func doneTappedButton(_ sender: Any) {
         if plasticTF.text!.isEmpty || glassTF.text!.isEmpty || paperTF.text!.isEmpty || metalTF.text!.isEmpty || organicTF.text!.isEmpty{
             print("Input The Data")
         }else{
             addData()
-            addDataTrash()
             UserDefaultService.instance.isTargetSet = true
             
             self.dismiss(animated: true) {
@@ -50,78 +42,31 @@ class SetTargetVC: UIViewController {
     }
     
     func addData(){
-        let catEntity = NSEntityDescription.entity(forEntityName: "CategoryCD", in: managedObjectContext!)
-        let plasticCat = NSManagedObject(entity: catEntity!, insertInto: managedObjectContext!)
-        let glassCat = NSManagedObject(entity: catEntity!, insertInto: managedObjectContext!)
-        let paperCat = NSManagedObject(entity: catEntity!, insertInto: managedObjectContext!)
-        let metalCat = NSManagedObject(entity: catEntity!, insertInto: managedObjectContext!)
-        let organicCat = NSManagedObject(entity: catEntity!, insertInto: managedObjectContext!)
-        
         //Plastic Category
-        plasticCat.setValue(Date(), forKey: "date")
         if let count = Int(plasticTF.text!){
-            plasticCat.setValue(count, forKey: "target")
+            presenter?.addCategoryTarget(name: "APlastic", target: count)
         }
-        plasticCat.setValue("APlastic", forKey: "name")
         
         //Glass Category
-        glassCat.setValue(Date(), forKey: "date")
         if let count = Int(glassTF.text!){
-            glassCat.setValue(count, forKey: "target")
+            presenter?.addCategoryTarget(name: "BGlass", target: count)
         }
-        glassCat.setValue("BGlass", forKey: "name")
         
         //Paper Category
-        paperCat.setValue(Date(), forKey: "date")
         if let count = Int(paperTF.text!){
-            paperCat.setValue(count, forKey: "target")
+            presenter?.addCategoryTarget(name: "CPaper", target: count)
         }
-        paperCat.setValue("CPaper", forKey: "name")
         
         //Metal CAtegory
-        metalCat.setValue(Date(), forKey: "date")
         if let count = Int(metalTF.text!){
-            metalCat.setValue(count, forKey: "target")
+            presenter?.addCategoryTarget(name: "DMetal", target: count)
         }
-        metalCat.setValue("DMetal", forKey: "name")
         
         //Organic Category
-        organicCat.setValue(Date(), forKey: "date")
         if let count = Int(organicTF.text!){
-            organicCat.setValue(count, forKey: "target")
+            presenter?.addCategoryTarget(name: "EOrganic", target: count)
         }
-        organicCat.setValue("EOrganic", forKey: "name")
         
-        do {
-            try managedObjectContext?.save()
-            print("saved \(plasticCat)")
-            print("saved \(glassCat)")
-            print("saved \(paperCat)")
-            print("saved \(metalCat)")
-            print("saved \(organicCat)")
-        }catch let error as NSError{
-            print("Couldnot save \(error)")
-        }
-    
-        }
-    
-    func addDataTrash(){
-            let trashEntity = NSEntityDescription.entity(forEntityName: "TrashCD", in: managedObjectContext!)
-            
-            for i in 0...4{
-                for n in 0...5{
-                    let newTrash = NSManagedObject(entity: trashEntity!, insertInto: managedObjectContext!)
-                    
-                    newTrash.setValue(Date(), forKey: "date")
-                    newTrash.setValue(dataWaste[i].categoryName, forKey: "type")
-                    newTrash.setValue(dataWaste[i].waste[n].wasteName, forKey: "name")
-                    newTrash.setValue(0, forKey: "count")
-                    do{
-                        try  managedObjectContext?.save()
-                    }catch let error as NSError{
-                        print("Couldnt Save Core Data")
-                    }
-                }
-            }
-        }
+        presenter?.addDataTrash()
+    }
 }
